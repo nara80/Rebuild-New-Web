d4234381bb02be46014f17a47a59352ef242fd00
MildMate Dev <nara19080@gmail.com>
2026-06-17 21:16:08 +0700

    Rewrite hero and description sections on accessories page (EN/TH)

--- public/_worker.js
+++ public/_worker.js
@@ -9368,7 +9368,7 @@ ${JSON_LD_WEBSITE}
 }
 __name(onRequest9, "onRequest");
 
-// ../.wrangler/tmp/pages-QArVQb/functionsRoutes-0.6018800029329624.mjs
+// ../.wrangler/tmp/pages-yGdNqh/functionsRoutes-0.478416345620406.mjs
 var routes = [
   {
     routePath: "/th/blogs/:path*",
